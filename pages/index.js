3faa555afa8b83a539c6ff439aebd79618796bdf
jpBorsato <48165246+jpBorsato@users.noreply.github.com>
2025-08-23 20:25:36 +0000

    add baby to `index.js`

--- pages/index.js
+++ pages/index.js
@@ -13,18 +13,40 @@ function Home() {
         height: "100vh",
       }}
     >
-      <img
-        src={liked ? "/heart_open.png" : "/heart_closed.png"}
-        alt={liked ? "Coração aberto" : "Coração fechado"}
+      <div
         style={{
-          cursor: "pointer",
-          width: "33vw",
-          height: "auto",
-          maxWidth: "100%",
-          maxHeight: "80vh",
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100vh",
         }}
-        onClick={() => setLiked(!liked)}
-      />
+      >
+        <img
+          src={liked ? "/baby1.png" : "/baby2.png"}
+          alt={liked ? "Bebê engatinhando..." : "Bebê engatinhando..."}
+          style={{
+            cursor: "pointer",
+            width: "16vw",
+            height: "auto",
+            maxWidth: "100%",
+            maxHeight: "100%",
+          }}
+          onClick={() => setLiked(!liked)}
+        />
+        <img
+          src={liked ? "/heart_open.png" : "/heart_closed.png"}
+          alt={liked ? "Coração aberto" : "Coração fechado"}
+          style={{
+            cursor: "pointer",
+            width: "33vw",
+            height: "auto",
+            maxWidth: "100%",
+            maxHeight: "80%%",
+          }}
+          onClick={() => setLiked(!liked)}
+        />
+      </div>
       <div
         style={{
           marginTop: "2rem",
