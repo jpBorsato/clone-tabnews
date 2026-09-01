bc33b1a37ef23bf6aab91d5438b240a7bd2445f9
jpBorsato <48165246+jpBorsato@users.noreply.github.com>
2026-09-01 00:22:05 +0000

    removes `dbClient.end()` redundancy

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -16,7 +16,6 @@ export default async function migrations(request, response) {
     };
     if (request.method === "GET") {
       const pendingMigrations = await migrationRunner(defaultMigrationsOptions);
-      await dbClient.end();
       return response.status(200).json(pendingMigrations);
     }
 
@@ -25,8 +24,6 @@ export default async function migrations(request, response) {
         ...defaultMigrationsOptions,
         dryRun: false,
       });
-
-      await dbClient.end();
 
       if (migratedMigrations.length > 0) {
         return response.status(201).json(migratedMigrations);
